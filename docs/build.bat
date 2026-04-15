627efb668437282e0377366d58e095d490cb1014
André Dietrich <andredietrich@web.de>
2026-04-15 11:56:38 +0200

    demo data added

---
+++ docs/build.bat
@@ -0,0 +1,11 @@
+liaex -f project -i index.yaml                         -o index
+liaex -f project -i arbeits-und-gesundheitsschutz.yaml -o arbeits-und-gesundheitsschutz --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i belehrungen.yaml                   -o belehrungen --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i maler.yaml                         -o maler --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i raumausstatter.yaml                -o raumausstatter --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i shk.yaml                           -o shk --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i sonstige.yaml                      -o sonstige --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i tischler.yaml                      -o tischler --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i umfragen.yaml                      -o umfragen --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i wissensspeicher.yaml               -o wissensspeicher --project-search --project-generate-pdf --project-generate-scorm12
+liaex -f project -i zahntechniker.yaml                 -o zahntechniker --project-search --project-generate-pdf --project-generate-scorm12
